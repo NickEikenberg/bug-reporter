b8492498dde731580a2b8b4a28dbdd45f3d96052
Nick Eikenberg <nickeikenberg95@gmail.com>
2022-01-10 15:05:14 -0500

    Added dropdown for bug assignee

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
   resources :projects
   devise_for :users
   resources :bugs
   # get 'home/index'
   root 'home#index'
+
+  match '/users',   to: 'users#index',   via: 'get'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
