c22d7fc07bdf8b55388ced25f1ae5824a05d89e6
Nick Eikenberg <nickeikenberg95@gmail.com>
2022-01-10 13:11:22 -0500

    switched db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_01_10_153500) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bugs", force: :cascade do |t|
     t.string "title"
     t.string "branch"
