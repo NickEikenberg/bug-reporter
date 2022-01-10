84b4a6df4b8da43ea32cc715a9bfc2583905ab2f
Nick Eikenberg <nickeikenberg95@gmail.com>
2022-01-10 15:41:50 -0500

    added show routes for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   get 'users/index'
   resources :projects
-  devise_for :users
+  devise_for :users, :path_prefix => 'd'
+  resources :users, :only =>[:show]
   resources :bugs
   # get 'home/index'
   root 'home#index'
 
   match '/users',   to: 'users#index',   via: 'get'
+  match '/users/:id', to: 'users#show', via: 'get'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
